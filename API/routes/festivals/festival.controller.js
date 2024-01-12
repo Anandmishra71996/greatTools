a046c9db0674aaf186f825b6549abb75ea60326c
Anand <anandmishra71996@gmail.com>
2024-01-12 20:21:00 +0530

    new route added

--- API/routes/festivals/festival.controller.js
+++ API/routes/festivals/festival.controller.js
@@ -1,12 +1,12 @@
 const Joi = require("Joi");
 const festivalModel = require("../../models/festivals/festival.model");
-const { festivalSchema } = require("../../joiModels/joiSchema");
+// const { festivalSchema } = require("../../joiModels/joiSchema");
 
 const festivalController = {
   createNewFestival: async (req, res) => {
     try {
       let body = req.body;
-      await Joi.valid(body, festivalSchema);
+      // await Joi.valid(body, festivalSchema);
       await festivalModel.addNewFestival(body);
       res.status(200).json({
         isSuccess: true,
